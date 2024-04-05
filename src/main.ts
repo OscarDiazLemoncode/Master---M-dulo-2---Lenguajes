a42e3eefbb8613e648baf8ec70e47df2a9c9fbc9
Oscar <trabajos1379@gmail.com>
2024-04-05 16:31:39 +0200

    saving

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,24 @@
 import './style.css';
 
-console.log('Hello Typescript!');
+interface Pelicula {
+  titulo: string;
+  director: string;
+  año: number;
+}
+
+const pelicula1: Pelicula = {
+  titulo: 'Blade Runner',
+  director: 'Ridley Scott',
+  año: 1982,
+};
+
+pelicula1.año = 2024;
+
+const pelicula2: Pelicula = {
+  titulo: 'Matrix',
+  director: 'Lana tattal',
+  año: 1999,
+};
+
+console.log(pelicula1);
+console.log(pelicula2);
