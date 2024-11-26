4010695a8332f15d38a6933c1442dd7f10a5cfc5
Oscar <trabajos1379@gmail.com>
2024-11-26 22:10:19 +0100

    implementado concat, concatOptional, clone y merge

--- src/main.ts
+++ src/main.ts
@@ -35,9 +35,48 @@ const init = (array: Books[]): Books[] => {
 init(books);
 
 // Last
-const last = (array: Books[]): Books[] => {
-  const lastItem = array.pop();
+const last = (array: Books[]): Books | undefined => {
+  const lastItem = [...array].pop();
   console.log(lastItem);
   return lastItem;
 };
 last(books);
+
+// Concat
+const array1 = ['one', 'two', 'three', 'four'];
+const array2 = [1, 2, 3, 4];
+const concat = <A, B>(a: A[], b: B[]): (A | B)[] => {
+  const merge = [...a, ...b];
+  console.log(merge);
+  return merge;
+};
+concat(array1, array2);
+
+// Optional
+const array3 = ['five', 'six', 'seven', 'eight'];
+function concatOptional<A>(): A[] {
+  let init: A[] = [];
+  for (let i = 0; i < arguments.length; i++) {
+    init += arguments[i];
+  }
+  console.log(init);
+  return init;
+}
+concatOptional(array1, array2, array3);
+
+// Clone Merge
+// Clone
+function clone(source: Books[]): Books[] {
+  console.log([...source]);
+  return [...source];
+}
+clone(books);
+
+// Merge
+const a = { name: 'Maria', surname: 'Ibañez', country: 'SPA' };
+const b = { name: 'Luisa', age: 31, married: true };
+// output=> {name: "Maria", age: 31, married: true, surname: "Ibañez", country: "SPA"}
+const merge = (source, target) => {
+  console.log({ ...target, ...source });
+};
+merge(a, b);
